5d487a85919c2668e504bafb914e96b37cff53fd
Ayub <ayubmja@icloud.com>
2024-03-01 10:27:25 -0500

    project 100% completed

--- server.py
+++ server.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template, request
+"""
+Emotion Detection Server
+
+This python script runs the flask server and evaluates the user input and returns the approx
+sentiment analysis done by the IBM AI.
+
+Author(Leaner): [ayubmja2]
+"""
+from flask import Flask,request,render_template
 from EmotionDetection.emotion_detection import emotion_detector
 from EmotionDetection.emotion_detection import emotion_predictor
 
 app = Flask("Emotion Detection")
 
 def run_server():
+    """
+    This the server port. This function get called on the bottom of the page.
+    """
     app.run(host="0.0.0.0", port=5000)
 
 #emotion detector decorator
 @app.route("/emotionDetector")
+
 def send_to_detector():
-    
+    """
+    This function sends the user provided input tot he detector function and return the results
+    """
     text_to_detect = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_detect)
     formated_response = emotion_predictor(response)
@@ -26,6 +40,9 @@ def send_to_detector():
 
 @app.route("/")
 def index_page():
+    """
+    This function renders the template page index.html on the "/" route
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
